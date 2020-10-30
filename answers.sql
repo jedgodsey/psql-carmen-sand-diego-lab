274d532b39d2d818a69313575269eba8b7491149
jedgodsey <jed.godsey@gmail.com>
2020-10-29 17:54:34 -0700

    Found Carmen Sandiego

--- answers.sql
+++ answers.sql
@@ -24,31 +24,48 @@ WHERE countrycode IN (
 -- to a different country, a country where people speak only the language she was learning. Find out which
 --  nearby country speaks nothing but that language.
 
+SELECT c.region, c.name, c.code FROM countrylanguage cl, country c
+WHERE language = 'Italian'
+AND cl.countrycode = c.code
+AND c.region = 'Southern Europe'
+AND 1 = (
+    SELECT COUNT(language) FROM countrylanguage
+    WHERE countrycode = c.code
+)
+AND c.code != 'VAT'
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time.
  -- There are only two cities she could be flying to in the country. One is named the same as the country – that
  -- would be too obvious. We're following our gut on this one; find out what other city in that country she might
  --  be flying to.
 
-
+ SELECT name, countrycode FROM city
+ WHERE countrycode IN (
+     SELECT code FROM country
+     WHERE name = 'San Marino'
+ )
+ AND name != 'San Marino'
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
 -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
+SELECT name, countrycode FROM city
+WHERE name LIKE 'Serra%'
+
+SELECT name FROM country
+WHERE code IN (
+    SELECT countrycode FROM city
+    WHERE name = 'Serra'
+)
 
 
--- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
- -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
- -- follow right behind you!
-
-
-
--- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
- -- the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the
- -- landing dock.
-
-
+-- Clue #6/#7: 
+SELECT name, countrycode FROM city
+WHERE id IN (
+    SELECT capital FROM country
+    WHERE name = 'Brazil'
+)
 
 -- Clue #8: Lucky for us, she's getting cocky. She left us a note, and I'm sure she thinks she's very clever, but
 -- if we can crack it, we can finally put her where she belongs – behind bars.
@@ -63,6 +80,9 @@ WHERE countrycode IN (
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
 
+SELECT name, countrycode FROM city
+WHERE population = 91084
 
 
--- She's in ______________________!
+
+She's in SANTA MONICA!
